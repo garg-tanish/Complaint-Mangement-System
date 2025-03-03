858ecbff418d6c3722e267679a3264010f3942e1
Tanish Garg <tanishgarg596@gmail.com>
2025-03-03 11:09:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import Home from "./components/Home/Home.js";
 import Auth from "./components/Auth/Auth.js";
 import Posts from "./components/Posts/Posts.js";
 import Navbar from "./components/Navbar/Navbar.js";
-import AdminAuth from "./components/Auth/AdminAuth.js";
 import Profile from './components/UserProfile/Profile.js'
 import PostDetails from "./components/Posts/PostDetails/PostDetails.js";
 
-import { Toaster } from 'react-hot-toast';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const [currentId, setCurrentId] = React.useState(0);
-
   const user = JSON.parse(localStorage.getItem("profile"));
 
   return (
@@ -28,31 +26,25 @@ const App = () => {
         <Switch>
 
           {
-            user && (
-              <>
-                <Route exact path="/">
-                  <Home currentId={currentId} />
-                </Route>
-                <Route exact path="/create">
-                  <Form setCurrentId={setCurrentId} />
-                </Route>
-                <Route exact path="/dashboard">
-                  <Posts setCurrentId={setCurrentId} />
-                </Route>
-                <Route exact path="/details/:id">
-                  <PostDetails />
-                </Route>
-                <Route exact path="/user-profile">
-                  <Profile />
-                </Route>
-                {
-                  user.result.isAdmin && (
-                    <Route exact path="/auth/admin">
-                      <AdminAuth />
-                    </Route>
-                  )}
-              </>
-            )}
+            user &&
+            <>
+              <Route exact path="/">
+                <Home currentId={currentId} />
+              </Route>
+              <Route exact path="/details/:id">
+                <PostDetails />
+              </Route>
+              <Route exact path="/user-profile">
+                <Profile />
+              </Route>
+              <Route exact path="/create">
+                <Form setCurrentId={setCurrentId} />
+              </Route>
+              <Route exact path="/dashboard">
+                <Posts setCurrentId={setCurrentId} />
+              </Route>
+            </>
+          }
 
           <Route path="*">
             <Auth />
