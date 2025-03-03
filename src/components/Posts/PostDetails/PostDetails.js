32b34be7f122a4cecec0d77adf0315b8d5a4ff52
Tanish Garg <tanishgarg596@gmail.com>
2025-03-03 11:09:24 +0530

    better UI

--- src/components/Posts/PostDetails/PostDetails.js
+++ src/components/Posts/PostDetails/PostDetails.js
@@ -1,53 +1,107 @@
+import React from "react";
 import moment from "moment";
 import useStyles from "./styles";
 
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 import {
+  Box,
   Grid,
-  Typography,
   Grow,
-  Container
+  Modal,
+  Button,
+  Container,
+  Typography,
+  IconButton
 } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { Close } from "@material-ui/icons";
+import { useParams } from "react-router-dom";
 
 const PostDetails = () => {
-
   const { id } = useParams();
   const classes = useStyles();
+
   const posts = useSelector((state) => state.postReducer);
   const post = posts.find((post) => post._id === id);
 
+  const [openImage, setOpenImage] = React.useState(false);
+
+  const handleClose = () => setOpenImage(false);
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="md" className={classes.mainContainer}>
         <Grid container justifyContent="center" alignItems="stretch">
-          <Grid item xs={12} sm={8}>
+          <Grid item xs={12}>
             {
-              posts && (
-                <Grid item lg={12} sm={8}>
-                  <Typography variant="h3" color="secondary" className={classes.title}>{post.title}</Typography>
-                  <Typography variant="h6" color="primary" className={classes.creator}>{post.creator}</Typography>
-                  <Typography variant="body2" className={classes.createdAt}>
+              post &&
+              <Grid spacing={2}>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="body2"
+                    color="Secondary"
+                    className={classes.createdAt}
+                  >
                     {moment(post.createAt).fromNow()}
                   </Typography>
-                  <Typography variant="body2" className={classes.content}>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h4" className={classes.title}>
+                    Title: {post.title}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="h6" className={classes.creator}>
+                    Registered By: {post.creator}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography
+                    variant="h3"
+                    className={classes.content}
+                  >
+                    Description of Complain:
+                  </Typography>
+                  <Typography variant="h4" >
                     {post.content}
                   </Typography>
-                  <Typography variant="body2" className={classes.content}>
-                    {post.feedback}
-                  </Typography>
                   {
-                    post.selectedFile && (
-                      <img
-                        src={post.selectedFile}
-                        alt="Uploaded Preview"
-                        style={{ width: "300px", height: "auto", marginTop: "10px" }}
-                      />
+                    post.feedback && (
+                      <Typography variant="body2" color="gray">
+                        Feedback: {post.feedback}
+                      </Typography>
                     )}
                 </Grid>
-              )}
+                {
+                  post.selectedFile &&
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    className={classes.mediaButton}
+                    onClick={() => setOpenImage(true)}
+                  >
+                    Show Media
+                  </Button>
+                }
+              </Grid>
+            }
           </Grid>
         </Grid>
+
+        <Modal open={openImage} onClose={handleClose}>
+          <Box className={classes.box}>
+            <IconButton
+              onClick={handleClose}
+              className={classes.closeButton}
+            >
+              <Close />
+            </IconButton>
+            <img
+              alt="Full Screen"
+              src={post.selectedFile}
+              style={{ maxWidth: "90%", maxHeight: "90%", borderRadius: "10px" }}
+            />
+          </Box>
+        </Modal>
       </Container>
     </Grow>
   );
--- src/components/Posts/PostDetails/styles.js
+++ src/components/Posts/PostDetails/styles.js
@@ -4,27 +4,46 @@ export default makeStyles((theme) => ({
   mainContainer: {
     display: 'flex',
     alignItems: 'center',
-  },
-  smMargin: {
-    margin: theme.spacing(1),
-  },
-  actionDiv: {
-    textAlign: 'center',
+    marginTop: '10px',
+    border: '2px solid black'
   },
   title: {
     marginTop: '20px',
     textAlign: 'center',
   },
   creator: {
-    textAlign: 'right',
+    textAlign: 'center',
   },
   createdAt: {
-    textAlign: 'right',
+    marginTop: 8,
+    textAlign: 'right'
   },
   content: {
-    marginTop: '25px',
+    textAlign: 'center',
     padding: '25px',
-    width: '100%',
-    height: 'auto',
+    marginTop: '25px',
+    marginBottom: '25px'
+  },
+  mediaButton: {
+    margin: 15
+  },
+  closeButton: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    "&:hover": { backgroundColor: "rgba(0, 0, 0, 0.8)" },
+  },
+  box: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
   }
 }));
